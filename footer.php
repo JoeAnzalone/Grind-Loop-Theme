ea32597ffa251973c741b5ed66d6accace062dc0
Joe Anzalone <joe@shmit.com>
2016-08-06 10:35:04 -0400

    Generate _s theme from http://underscores.me

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Grind_Loop
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'grind-loop' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'grind-loop' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'grind-loop' ), 'grind-loop', '<a href="http://JoeAnzalone.com" rel="designer">Joe Anzalone</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
